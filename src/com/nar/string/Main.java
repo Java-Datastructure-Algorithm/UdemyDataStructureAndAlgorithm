0fca75597b86da1de2fac50e47d8c24213b3b1e9
Narendra Nirala <nknirala.007@gmail.com>
2019-03-18 18:10:16 +0530

    String operations

--- src/com/nar/string/Main.java
+++ src/com/nar/string/Main.java
@@ -8,30 +8,49 @@ public class Main {
 
         //----------Theory is there in separate document
 
-        //---------Substring Example
+        System.out.println("-----------Anargam Check------------");
+        System.out.println("is Anargam :"+stringOperations.isAnagram("SILENT", "LISTEN"));
+
+        System.out.println("---------Substring Example----------");
         // .substring prints till index  : endindex -1
         System.out.println("012345678".substring(0,8));//Will print till 8-1=7th index
         System.out.println("012345678".substring(0,1)); //First character
 
-        //-------Reverse String O(N) complexity
-        System.out.println(stringOperations.reverseString("narendra"));
+        System.out.println("-------Reverse String O(N) complexity---------");
+        System.out.println("Using StringBuilder Append: "+stringOperations.reverseString("narendra"));
+        System.out.println("Using recursive function: "+stringOperations.reverseStringRecursively("narendra"));
+        //There is no inplace reverse method since there is no setCharAt method
 
-        System.out.println(new StringBuilder("narendra").reverse());
 
-        //----------Suffixes
+        System.out.println("using StringBuilder reverse method:"+new StringBuilder("narendra").reverse());
+
+        System.out.println("----------Suffixes----------");
         List<String> suffixes = stringOperations.getSuffixes("abcde");
         System.out.println("suffixes: "+suffixes);
 
-        //---------Prefixes
+        System.out.println("---------Prefixes----------");
         List<String> prefixes = stringOperations.getPrefixes("abcde");
         System.out.println("prefixes: "+prefixes);
 
-        //-----------Longest Common Prefix
+        System.out.println("-----------Longest Common Prefix----------");
         String lonestCommonPrefix = stringOperations.getLongestCommonPrefix("abcde", "abcdefg");
         System.out.println("longestcommonPrefix: "+lonestCommonPrefix);
 
-        //------------Longest Repeating Substring
+        System.out.println("----------Longest Repeating Substring-----");
         String longestRepeatingSubStr = stringOperations.getLongestRepeatedSubstring("hellloabcdehello");
         System.out.println("longestRepeatingSubstr: "+longestRepeatingSubStr);
+
+        System.out.println("-----------Check String contains only digit-------");
+        System.out.println("1234 isStrinContainsOnlyDigit: "+stringOperations.isContainOnlyDigits("1234"));
+        System.out.println("1234a isStrinContainsOnlyDigit: "+stringOperations.isContainOnlyDigits("1234a"));
+
+        System.out.println("-----------Check String contains only specified no of digits-------");
+        System.out.println("123456 isStrinContainsOnly6Digit: "+stringOperations.isContainingSpecifiedDigits("123456", 6));
+        System.out.println("1234567 isStrinContainsOnly6Digit: "+stringOperations.isContainingSpecifiedDigits("1234567", 6));
+        System.out.println("12345ab isStrinContainsOnly6Digit: "+stringOperations.isContainingSpecifiedDigits("12345ab", 6));
+
+        System.out.println("--------Check String Rotation-----------");
+        System.out.println(stringOperations.isRotationString("INDIAUSAJAPAN", "USAJAPANINDIA"));
+
      }
 }
--- src/com/nar/string/StringOperations.java
+++ src/com/nar/string/StringOperations.java
@@ -1,10 +1,34 @@
 package com.nar.string;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class StringOperations {
+
+    //------------Anargam check
+    // Anargam LISTEN - SILENT, TRIANGLE - INTEGRAL
+    public boolean isAnagram(String str1 , String str2){
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        if(arr1.length != arr2.length){
+            return false;
+        }
+        // O(NlogN) running time
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        //running time is O(N)
+        //O(NlogN)+O(N) = O(NlogN)
+        for(int i = 0; i<arr1.length; i++){
+            if(arr1[i] != arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
 
     //-----------Reverse String
     public String reverseString(String text){
@@ -17,6 +41,13 @@ public class StringOperations {
         return sb.toString();
     }
 
+    public String reverseStringRecursively(String text){
+        if(text.length() == 1){//or <2
+            return text;
+        }
+
+        return (reverseStringRecursively(text.substring(1)) + text.charAt(0));
+    }
 
     //----------Suffixes
     public List<String> getSuffixes(String text){
@@ -75,5 +106,36 @@ public class StringOperations {
         return longestSubstring;
     }
 
+    public boolean isContainOnlyDigits(String text){
+        /*The Java Pattern class can be used in two ways. You can use the Pattern.matches()
+        method to quickly check if a text (String) matches a given regular expression.
+        Or you can compile a Pattern instance using Pattern.compile() which can be used multiple
+        times to match the regular expression against multiple texts.
+        Both the Pattern.matches() and Pattern.compile() methods are covered below.*/
+
+
+        Pattern pattern = Pattern.compile(".*[^0-9].*");
+        //Pattern pattern = Pattern.compile(".*\\D.*");
+        return !pattern.matcher(text).matches(); // Here notice ! operator
+    }
+
+    public boolean isContainingSpecifiedDigits(String text, int noOfDigits){
+        Pattern digitPattern = Pattern.compile("\\d{"+noOfDigits+"}");
+        return digitPattern.matcher(text).matches();
+    }
+
+    public boolean isRotationString (String str1, String str2){
+        if(str1.length() != str2.length()){
+            return false;
+        }
+
+        String concatenated = str1 + str1;
+
+        if(concatenated.indexOf(str2) != -1){
+            return true;
+        }
+
+        return false;
+    }
 
 }
